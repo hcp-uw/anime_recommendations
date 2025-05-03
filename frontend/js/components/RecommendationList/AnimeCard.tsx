e762901f8925d474b8751c73f0f740b8eee41c3f
Sofia Fristrom <sofia.p.fristrom@gmail.com>
2025-05-02 23:04:39 -0700

    Updated UI of main parts of anime card

--- frontend/js/components/RecommendationList/AnimeCard.tsx
+++ frontend/js/components/RecommendationList/AnimeCard.tsx
@@ -3,58 +3,104 @@ import { Card, Row, Col, Badge } from "react-bootstrap";
 
 import { malCategories } from "../../constants";
 import { Anime } from "../../types";
+import { MalCategoryId } from "../../types";
 
 interface AnimeCardProps {
   anime: Anime;
 }
 
-const AnimeCard: React.FC<AnimeCardProps> = ({ anime }) => {
+function AnimeCard({ anime } : AnimeCardProps ) {
   return (
     <Card className="mb-3">
-      <Row noGutters>
-        {/* Image Section */}
-        <Col md={2}>
-          <Card.Img alt={anime.name} className="h-100" src={anime.imageUrl} />
-        </Col>
-
-        {/* Details Section */}
-        <Col md={10}>
-          <Card.Body>
-            <Row>
-              {/* Anime Title and Genres */}
-              <Col md={3}>
-                <Card.Title>{anime.name}</Card.Title>
-                <div className="mb-2">
-                  {anime.genres.map((genre) => (
-                    <Badge key={genre} bg="secondary" className="mr-1">
-                      {malCategories[genre]}
-                    </Badge>
-                  ))}
-                </div>
-              </Col>
-
-              {/* Synopsis */}
-              <Col md={7}>
-                <Card.Text className="text-truncate">
-                  {anime.synopsis}
-                </Card.Text>
-              </Col>
-
-              {/* Action Button or Status */}
-              <Col
-                className="d-flex align-items-center justify-content-center"
-                md={2}
-              >
-                <Badge bg="success" pill>
-                  Plan To Watch
-                </Badge>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Col>
+      <Row className="g-2">
+        <ImageSection anime={anime} />
+        <MainSection anime={anime} />
+        <DetailsSection anime={anime} />
       </Row>
     </Card>
   );
-};
+}
+
+function ImageSection({ anime } : AnimeCardProps) {
+  return (
+    <Col md={2}>
+      <Card.Img alt={anime.name} className="h-100" src={anime.imageUrl} />
+    </Col>
+  );
+}
+
+function MainSection({ anime } : AnimeCardProps) {
+  return (
+    <Col md={8}>
+      <Card.Body >
+        <Title anime={anime} />
+        <Genres anime={anime} />
+        <Synopsis anime={anime} />
+      </Card.Body>
+    </Col>
+  );
+}
+
+
+function Title ( {anime} : AnimeCardProps ) {
+  return (
+    <Row >
+      <Card.Title>
+        <Card.Link href={anime.malUrl} target="_blank" className="text-reset text-decoration-none">{anime.name}</Card.Link>
+      </Card.Title>
+    </Row>
+  );
+}
+
+
+function Genres ( {anime} : AnimeCardProps ) {
+  return (
+    <Row className="py-1">
+      <div className="d-flex flex-padding">
+        {anime.genres.map((genre) => ( <Genre genre={genre} />))}
+      </div>
+    </Row>
+  )
+}
+
+
+function Genre ( {genre} : { genre: MalCategoryId } ) {
+  return ( 
+    <Badge key={genre} bg="custom-blue" className="mr-1 rounded align-items-center px-2 gap-2">
+      {malCategories[genre]}
+    </Badge>
+  );
+}
+
+function Synopsis ( {anime} : AnimeCardProps ) {
+  return (
+    <Row>    
+      <Card.Text className="text-truncate-container">
+        {anime.synopsis}
+      </Card.Text>
+    </Row>
+  )
+}
+
+function DetailsSection({ anime } : AnimeCardProps) {
+  return (
+    <Col
+      className="d-flex justify-content-center pe-4"
+      md={2} 
+    >
+      <Card.Body>
+      <Row >
+        <Badge bg="success" pill>
+          Plan to Watch
+        </Badge>
+      </Row>
+      <Row>
+        Text 
+      </Row>
+
+      </Card.Body>
+    </Col>
+  );
+}
 
 export default AnimeCard;
--- frontend/js/constants/dummyAnimeList.ts
+++ frontend/js/constants/dummyAnimeList.ts
@@ -4,7 +4,7 @@ export const dummyAnimeList: Anime[] = [
   {
     id: 11061,
     name: "Hunter x Hunter (2011)",
-    synopsis: "Gon Freecss sets out on a journey to become a Hunter and find his missing father.",
+    synopsis: "Hunters devote themselves to accomplishing hazardous tasks, all from traversing the world's uncharted territories to locating rare items and monsters. Before becoming a Hunter, one must pass the Hunter Examination—a high-risk selection process in which most applicants end up handicapped or worse, deceased. \nAmbitious participants who challenge the notorious exam carry their own reason. What drives 12-year-old Gon Freecss is finding Ging, his father and a Hunter himself. Believing that he will meet his father by becoming a Hunter, Gon takes the first step to walk the same path. \nDuring the Hunter Examination, Gon befriends the medical student Leorio Paladiknight, the vindictive Kurapika, and ex-assassin Killua Zoldyck. While their motives vastly differ from each other, they band together for a common goal and begin to venture into a perilous world. \n[Written by MAL Rewrite]",
     malUrl: "https://myanimelist.net/anime/11061/Hunter_x_Hunter_2011",
     imageUrl: "https://cdn.myanimelist.net/images/anime/1537/145325.jpg",
     genres: [1, 2, 3, 4],
